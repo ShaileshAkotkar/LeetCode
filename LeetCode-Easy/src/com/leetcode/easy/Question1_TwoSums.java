3dddecc4aa4ef0ea552d15af3d0235b9140457db
Shailesh Akotkar <sa55114@imcnam.ssmb.com>
2018-01-29 15:49:06 -0500

    - adding one shot code

--- LeetCode-Easy/src/com/leetcode/easy/Question1_TwoSums.java
+++ LeetCode-Easy/src/com/leetcode/easy/Question1_TwoSums.java
@@ -1,6 +1,7 @@
 package com.leetcode.easy;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*Question:
 Given nums = [2, 7, 11, 15], target = 9,
@@ -13,24 +14,32 @@ public class Question1_TwoSums {
 	public int[] twoSum(int[] nums, int target) {
 
 		int size = nums.length;
-		int i = 0, j=1;
-
+		System.out.println(size);
 		if (size < 2)
 			return new int[2];
-
-		for (; i <= size - 2; i++) {
-			for (; j < size - 1; j++) {
+		for (int i = 0; i <= size - 2; i++) {
+			for (int j = i + 1; j <= size - 1; j++) {
 				int temp = nums[i] + nums[j];
-				//System.out.println(temp +" "+ i + " " + j);
 				if (temp == target) 
 					return (new int[]{i ,j});
 				if (temp > target)
 					break;
 			}
-
 		}
-
-		return (new int[]{i ,j});
+		return (new int[]{0,0});
+	}
+	
+	
+	public int[] twoSumOneShot(int[] nums, int target) {
+		
+		Map<Integer, Integer> hashMap = new HashMap<>();
+		
+		for (int i = 0; i < nums.length; i++) {
+			if (hashMap.containsKey(nums[i]))
+				return new int[]{hashMap.get(nums[i]), i};
+			hashMap.put(target - nums[i], i);			
+		}
+		throw new IllegalArgumentException("Not a solution");
 	}
 
 	public static void main(String[] args) {
@@ -38,7 +47,7 @@ public class Question1_TwoSums {
 		int num[] = { 2, 7, 11, 15 };
 		int target = 22;
 
-		int[] twoSum = new Question1_TwoSums().twoSum(num, target);
+		int[] twoSum = new Question1_TwoSums().twoSumOneShot(num, target);
 
 		System.out.println(twoSum[0] + " " + twoSum[1]);
 		System.out.println(num[twoSum[0]] + " " + num[twoSum[1]]);
